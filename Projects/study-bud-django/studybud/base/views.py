312e237d8606e1fd11cbf03d66e02c6e4c4b402a
seanarch <zhuqingyun@outlook.com>
2023-06-21 23:59:20 -0600

    render views contents in home

--- Projects/study-bud-django/studybud/base/views.py
+++ Projects/study-bud-django/studybud/base/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Design with me!'},
+    {'id': 3, 'name': 'Frontend developers!'},
+]
+
 # Create your views here.
 def home(request): 
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 def room(request):
     return render(request, 'room.html')
